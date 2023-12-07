df272125a57fc519a75e88db77ce78dda6d3f640
elsenorbw <nope@nowhere.com>
2023-12-07 23:16:31 +0000

    Updated the template to include splitOn' and allSame

--- template/dayXXp1.hs
+++ template/dayXXp1.hs
@@ -2,3 +2,15 @@ main = interact mainFunc
 
 mainFunc :: String -> String
 mainFunc input = input
+
+-- Utility functions
+splitOn' :: (Eq a) => a -> [a] -> [[a]]
+splitOn' _ [] = []
+splitOn' delim x =
+  let (p1, p2) = break (== delim) x
+      remaining = drop 1 p2
+   in p1 : splitOn' delim remaining
+
+allSame :: (Eq a) => [a] -> Bool
+allSame [x] = True
+allSame (x : xs) = if x == head xs then allSame xs else False
